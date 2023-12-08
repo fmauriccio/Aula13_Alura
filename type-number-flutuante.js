76585edf2d43d86471908f50259a8a7e7e0eaef4
fmauriccio <88916124+fmauriccio@users.noreply.github.com>
2023-12-08 18:21:36 -0300

    Add files via upload

---
+++ type-number-flutuante.js
@@ -0,0 +1,8 @@
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao)
---
+++ type-string.js
@@ -0,0 +1,7 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = 'O Leo disse "oi!"'
+console.log(citacao)
