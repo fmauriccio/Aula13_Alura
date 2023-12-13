2d286d445d89909f840a89de09734181336719b5
fmauriccio <88916124+fmauriccio@users.noreply.github.com>
2023-12-13 16:25:09 -0300

    Aula 02

---
+++ null-undefined.js
@@ -0,0 +1,17 @@
+// undefined
+// null => vazio ou nada
+
+let minhaVar;
+let varNull = null
+
+console.log(minhaVar);
+console.log(varNull);
+
+let numero = 3;
+let texto = "Altura";
+
+console.log(typeof numero);
+console.log(typeof texto);
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
