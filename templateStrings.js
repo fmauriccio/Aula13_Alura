f51d06e2305a4d20045a20e94e1816fc33b0b2d5
fmauriccio <88916124+fmauriccio@users.noreply.github.com>
2023-12-16 14:14:10 -0300

    Aula 04

---
+++ templateStrings.js
@@ -0,0 +1,9 @@
+const nome = "Fernando";
+const idade = 2023-1996;
+const cidadeDeNascimento = "Valença"
+
+//const apresentacao = "Meu nome é " + nome + ", tenho " + idade + " anos e nasci na cidade de " + cidadeDeNascimento + ".";
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(apresentacao);
